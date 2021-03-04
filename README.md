96931f8f4bc711e3f9fabc60644259e156c02533
sheltsnn15 <76778791+sheltsnn15@users.noreply.github.com>
2021-03-04 10:32:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Winning-Smile-Website
+ Winning Smiles Website - Web Development Assignment 1 Basic HTML and CSS
---
+++ script.js
@@ -0,0 +1,8 @@
+
+function show_hide(show, hide)
+{// show and hide locations on click event
+document.getElementById(show).style.display = "block";
+document.getElementById(hide).style.display = "none";
+}
+
+
